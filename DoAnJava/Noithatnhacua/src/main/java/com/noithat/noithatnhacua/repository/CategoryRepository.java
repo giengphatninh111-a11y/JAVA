7bfe9dcf40fc5b2fcd05f5999afaa369c8856dca
Karma <giengphatninh111@gmail.com>
2026-06-10 18:01:19 +0700

    Delete DoAnJava/Noithatnhacua directory

--- DoAnJava/Noithatnhacua/src/main/java/com/noithat/noithatnhacua/repository/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.noithat.noithatnhacua.repository;
-
-import com.noithat.noithatnhacua.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-}
--- DoAnJava/Noithatnhacua/src/main/java/com/noithat/noithatnhacua/repository/OrderDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.noithat.noithatnhacua.repository;
-
-import com.noithat.noithatnhacua.model.OrderDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
-    List<OrderDetail> findByOrderId(Long orderId);
-}
